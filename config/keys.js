9cf929479e715d22e2b1ef9fca33d1b35f07c0ab
aeromel.laure@student.passerellesnumeriques.org <aeromel.laure@student.passerellesnumeriques.org>
2020-05-29 15:03:56 +0800

    connected to atlas

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,10 @@
 module.exports = {
     mongoURI: 'mongodb://127.0.0.1:27017/sfos',
-    atlasURI: "mongodb+srv://jean:jeanpassword@cluster0-chvj2.azure.mongodb.net/abang?retryWrites=true&w=majority",
+    atlasURI: "mongodb://user_11:ilovefood@sfos-shard-00-00-3myv5.mongodb.net:27017,sfos-shard-00-01-3myv5.mongodb.net:27017,sfos-shard-00-02-3myv5.mongodb.net:27017/test?ssl=true&replicaSet=sfos-shard-0&authSource=admin&retryWrites=true&w=majority",
     secretOrKey: "secret",
-    sercet :"madam1234"
+    // sercet :"madam1234"
 };
+
+
+
+// ilovefood
--- setup/mongo.js
+++ setup/mongo.js
@@ -3,7 +3,7 @@ const _CONFIG = require("../config/keys")
 console.log("connecting");
 mongoose.Promise = global.Promise;
 
-mongoose.connect(_CONFIG.mongoURI, {
+mongoose.connect(_CONFIG.atlasURI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
