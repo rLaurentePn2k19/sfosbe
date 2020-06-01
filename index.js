573d85e58c41493c2f39f1f81b998395d782f334
aeromel.laure@student.passerellesnumeriques.org <aeromel.laure@student.passerellesnumeriques.org>
2020-06-01 11:11:48 +0800

    Updated Generated Port env

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 const express = require('express')
 const app = express()
-const port = 4000
+const port =  process.env.PORT || 4000
 var bodyParser = require('body-parser')
 var cors = require('cors')
 var path = require('path')
 
-// var schedule = require('node-schedule');
-
-// var rule = new schedule.RecurrenceRule();
-// rule.second = 3
-
-// schedule.scheduleJob(rule, function () {
-//     console.log('The answer to life, the universe, and everything!');
-// });
+// var corsOptions = {
+//     origin: 'https://sfos-backend.herokuapp.com',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+//   }
 
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(bodyParser.json());
@@ -24,6 +20,7 @@ require('./setup/mongo');
 
 const order = require('./routes/user_routes');
 const admin = require('./routes/admin_routes');
+
 app.use('/order', order);
 app.use('/admin', admin);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
